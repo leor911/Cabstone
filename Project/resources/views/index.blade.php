08555d7e795bee89ddbf4a4d0cd63e101a551f4e
Coopcat164 <coop.rhrt@gmail.com>
2024-02-09 14:38:39 -0500

    small tweaks

--- Project/resources/views/index.blade.php
+++ Project/resources/views/index.blade.php
@@ -56,6 +56,15 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarCollapse">
                     <div class="navbar-nav ms-auto">
+                        <div class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Profile</a>
+                            <div class="dropdown-menu rounded-0 m-0">
+                                <a href="" class="dropdown-item">First name here</a>
+                                <a href="" class="dropdown-item">Last name here</a>
+                                <a href="" class="dropdown-item">Email here</a>
+                                <a href="" class="dropdown-item">Sign Out</a>
+                            </div>
+                        </div>
                         <a href="index.html" class="nav-item nav-link active">Home</a>
                         <a href="about.html" class="nav-item nav-link">About</a>
                         <div class="nav-item dropdown">
