cfd3c2ef083d9583b5527f96fc9b99880482393e
CSET5225 <KorosecCSET@outlook.com>
2024-02-23 12:27:43 -0500

    Titles
    I replaced all the Makaan titles with the actual content for that page

--- Project/resources/views/map.blade.php
+++ Project/resources/views/map.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Map</title>
+    <title>Phillow - Map</title>
     
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"/>
     <style>
